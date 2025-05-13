6471f95f98788108bf35690ad1804e89d529064b
FernandaRobayo <mfrobayo-2023a@corhuila.edu.co>
2025-05-12 19:54:52 -0500

    add functions

--- src/main/java/com/BackPM/BackPM/controllers/MesasController.java
+++ src/main/java/com/BackPM/BackPM/controllers/MesasController.java
@@ -1,5 +1,6 @@
 package com.BackPM.BackPM.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import com.BackPM.BackPM.models.Mesas;
 import com.BackPM.BackPM.services.IService.IMesaService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/mesas")
 public class MesasController extends ABaseController<Mesas, IMesaService> {
 
--- src/main/java/com/BackPM/BackPM/controllers/ReservaController.java
+++ src/main/java/com/BackPM/BackPM/controllers/ReservaController.java
@@ -1,5 +1,6 @@
 package com.BackPM.BackPM.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import com.BackPM.BackPM.models.Reservas;
 import com.BackPM.BackPM.services.IService.IReservaService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/reservas")
 public class ReservaController extends ABaseController<Reservas, IReservaService> {
 
